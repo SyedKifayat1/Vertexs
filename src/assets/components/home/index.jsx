be56f8059e1633a125ac1402216556fd2aa76e67
Syed Kifayat <bsce22026@itu.edu.pk>
2024-09-09 12:36:31 +0500

    commit

--- src/assets/components/home/index.jsx
+++ src/assets/components/home/index.jsx
@@ -1,8 +1,33 @@
 import { useRef, useState } from 'react';
 import './home.css'; // Ensure this CSS file includes the necessary styles
 import Banner from "../../gallery/virtual-assistant-team-meetup.webp";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import Company1 from "../../gallery/companies/company-1.webp";
+import Company2 from "../../gallery/companies/company-3.webp";
+import Company3 from "../../gallery/companies/company-4.webp";
+import Company4 from "../../gallery/companies/company-5.webp";
+import Company5 from "../../gallery/companies/company-6.webp";
+import Company6 from "../../gallery/companies/company-7.webp";
+import Company7 from "../../gallery/companies/company-2.webp";
+
+import { Autoplay } from 'swiper/modules';
 
 const Home = () => {
+    const companyImages = [
+        Company1,
+        Company2,
+        Company3,
+        Company4,
+        Company5,
+        Company6,
+        Company7,
+        Company2,  // Repeat Company2 if you want to alternate or loop it again
+        Company4,  // You can alternate however you'd like
+        Company1   // Continue alternating
+    ];
     const [modalOpen, setModalOpen] = useState(false);
     const modalRef = useRef(null);
     const offset = useRef({ x: 0, y: 0 });
@@ -38,8 +63,6 @@ const Home = () => {
         }
     };
 
-    const [rating, setRating] = useState(4);
-
     return (
         <div className="home-main">
             <div className="home-details">
@@ -60,7 +83,7 @@ const Home = () => {
                                 return (
                                     <span
                                         key={index}
-                                        className={`star ${starIndex <= rating ? 'filled' : ''}`}
+                                        className={`star ${starIndex <= 4 ? 'filled' : ''}`}
                                     >
                                         &#9733;
                                     </span>
@@ -83,14 +106,6 @@ const Home = () => {
             </div>
             <div className="home-video">
                 <div className="video-container">
-                    {/* <iframe
-                        id="video-frame"
-                        src="https://www.youtube.com/embed/sQEeFi6k2EY"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Video"
-                    ></iframe> */}
                     <div className="banner" onClick={handleBannerClick}>
                         <img src={Banner} alt="Banner" className="banner-img" />
                         <div className="play-button">
@@ -121,11 +136,39 @@ const Home = () => {
                 )}
 
                 <div className="companies-slider">
-                    {/* Add slider content here */}
+                    <Swiper
+
+                        spaceBetween={10} // Space between slides
+                        slidesPerView={5} // Show 4 slides at a time
+                        autoplay={{
+                            delay: 0, // No delay, continuous effect
+                            disableOnInteraction: false,
+                        }}
+                        speed={5000} // Transition speed, smooth sliding
+                        loop={true} // Infinite loop
+                        modules={[Autoplay]}
+                        className="mySwiper"
+                    >
+                        {/* <SwiperSlide><img src={Banner} alt="Slide 1" /></SwiperSlide>
+                        <SwiperSlide><img src={Banner} alt="Slide 2" /></SwiperSlide>
+                        <SwiperSlide><img src={Banner} alt="Slide 3" /></SwiperSlide>
+                        <SwiperSlide><img src={Banner} alt="Slide 4" /></SwiperSlide>
+                        <SwiperSlide><img src={Banner} alt="Slide 5" /></SwiperSlide>
+                        <SwiperSlide><img src={Banner} alt="Slide 6" /></SwiperSlide>
+                        <SwiperSlide><img src={Banner} alt="Slide 7" /></SwiperSlide>
+                        <SwiperSlide><img src={Banner} alt="Slide 8" /></SwiperSlide> */}
+                        {companyImages.map((company, index) => (
+                            <SwiperSlide key={index}>
+                                <img src={company} alt={`Company ${index + 1}`} />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
     );
 };
 
+
 export default Home;
+
