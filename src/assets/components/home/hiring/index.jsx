7dd7a9fbf5331fc861a194ace817753fac113948
Syed Kifayat <bsce22026@itu.edu.pk>
2024-09-13 15:45:19 +0500

    commit

---
+++ src/assets/components/home/hiring/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Hiring = () => {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <div>
+                    <h2>Tired of The Hiring Gamble?</h2>
+                    <h3>Imagine scaling without the headache.</h3>
+                    <p>
+                        Our virtual assistants are pre-trained and ready to support your digital marketing needs. Trusted by leading agencies, they’re ready to contribute to your success. Whether it’s SEO, content creation, social media management, or analytics, they’re ready to jump in and make a tangible difference in your day-to-day giving you your time back.
+                    </p>
+                    <h4>
+                        Did you know virtual assistants …
+                    </h4>
+                </div>
+                <div>
+                    <div>
+                        <div className="icon"></div>
+                        <div className="icon-detials">Boost Retention</div>
+                    </div>
+                    <div>
+                        <div className="icon"></div>
+                        <div className="icon-detials">One VA = $300K Gain</div>
+                    </div>
+                    <div>
+                        <div className="icon"></div>
+                        <div className="icon-detials">Cut Payroll by 60%</div>
+                    </div>
+                </div>
+                <div>
+                    <button>Virtual Assistant Roles</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Hiring
